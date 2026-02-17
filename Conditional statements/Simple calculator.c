38e9646e9a6bbee1d9d896a7c86a9733f8f7c9de
mahasrivelan47-cyber <mahasrivelan47@gmail.com>
2026-02-17 11:44:49 +0530

    Add simple calculator using conditional statements

---
+++ Conditional statements/Simple calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    int a, b;
+
+    printf("Enter operator (+ - * /): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch (op) {
+        case '+': printf("Result = %d", a + b); break;
+        case '-': printf("Result = %d", a - b); break;
+        case '*': printf("Result = %d", a * b); break;
+        case '/': printf("Result = %d", a / b); break;
+        default: printf("Invalid operator");
+    }
+
+    return 0;
+}
