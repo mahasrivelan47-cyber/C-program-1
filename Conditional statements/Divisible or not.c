8797be958435291b51e2e88d47e87ac7f5834289
mahasrivelan47-cyber <mahasrivelan47@gmail.com>
2026-02-18 13:40:55 +0530

    Create Divisible or not.c

---
+++ Conditional statements/Divisible or not.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, divisor;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    if (divisor == 0)
+        printf("Error! Division by zero is not allowed.");
+    else if (num % divisor == 0)
+        printf("Divisible");
+    else
+        printf("Not Divisible");
+
+    return 0;
+}
