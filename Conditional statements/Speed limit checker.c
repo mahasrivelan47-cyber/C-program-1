322a708d40a9a805d2a6dba10b7fb656c770e53c
mahasrivelan47-cyber <mahasrivelan47@gmail.com>
2026-02-18 13:48:30 +0530

    Add speed limit checker program

---
+++ Conditional statements/Speed limit checker.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int speedLimit, vehicleSpeed;
+
+    printf("Enter speed limit: ");
+    scanf("%d", &speedLimit);
+
+    printf("Enter vehicle speed: ");
+    scanf("%d", &vehicleSpeed);
+
+    if (vehicleSpeed > speedLimit)
+        printf("Warning! Speed limit exceeded.");
+    else
+        printf("Within speed limit.");
+
+    return 0;
+}
