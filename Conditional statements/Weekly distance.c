e9d3928f14ecd4a7b32ab2c31c36ad4b42a60783
Maha Sri M R <mahasrivelan47@gmail.com>
2026-02-28 11:56:05 +0530

    Add Weekly distance program to calculate distance

---
+++ Conditional statements/Weekly distance.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float x, total;
+
+    scanf("%f", &x);
+
+    total = x * 2 * 5;
+
+    printf("Total distance in a week = %.2f km\n", total);
+
+    return 0;
+}
