87a37655917ba51ad59e20a98133d0f696793171
Maha Sri M R <mahasrivelan47@gmail.com>
2026-02-28 15:36:00 +0530

    Create Power of 2 (512–1024).c

---
+++ Conditional statements/Power of 2 (512–1024).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 512 && n <= 1024 && (n & (n - 1)) == 0)
+        printf("Power of 2");
+    else
+        printf("Not Power of 2");
+
+    return 0;
+}
